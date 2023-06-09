f8f6f2011dead3d3b0413c0da98422f2331888f0
Junior Martins <amjr.box@gmail.com>
2023-06-09 09:58:55 -0300

    feat: implementa a page principal do app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-import Image from 'next/image'
+import NavigationMenu from './components/navigation-menu'
+import NavTop from './components/nav-top'
 
 export default function Home() {
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/horus53_logo_horizontal.svg'
-          alt='Next.js Logo'
-          width={554}
-          height={59}
-          priority
-        />
-      </div>
-    </main>
-  )
+  return <h1>Página inicial</h1>
 }
